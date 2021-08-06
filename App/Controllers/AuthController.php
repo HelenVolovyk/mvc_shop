15e39ba16e628789d00d3ad8e230430e03da294a
Helen Volovyk <mr0778240@gmail.com>
2021-08-06 19:34:36 +0300

    authcontroller

--- App/Controllers/AuthController.php
+++ App/Controllers/AuthController.php
@@ -5,13 +5,14 @@ use App\Models\User;
 use Framework\Core\AbsController;
 use Framework\Core\AbsView;
 use Framework\Helpers\SessionHelpers;
+use Framework\Session\Session;
 
 class AuthController extends AbsController
 {
 
+
 	public function login()
     {
-		
       AbsView::render('auth/login.php');
     }
 
@@ -21,50 +22,30 @@ class AuthController extends AbsController
 	 }
 
 
-	 public function isAuth()
-	 {
-		 SessionHelpers::isUserLogin();
-	 }
-	 
 	 
 	public function auth()
 	{
-	
-		// unset($_SESSION['error']['login']['common']);
-		$fields = filter_input_array(INPUT_POST, $_POST, 1);
+		unset($_SESSION['error']['login']['common']);
 
+		$fields = filter_input_array(INPUT_POST, $_POST, 1);
 		$user = new User();
 		
 		if ($userData = $user->getUserByEmail($fields['email'])){
 
 			if(password_verify($fields['pass'], $userData['pass'])){
-							
-				SessionHelpers::setUserData('id', $userData['id']);
-				SessionHelpers::setUserData('name', $userData['name']);
-				
+				Session::setName($userData['name']);
 				AbsView::site_redirect('/');
-				} 
+			} 
 		} else{
-		
-		$_SESSION['error']['login']['common'] = 'Data is not correct';
-		
+			$_SESSION['error']['login']['common'] = 'Data is not correct';
 		}
-
-	AbsView::site_redirect('/login');
-
-
- }
+	 }
+	 
+	 
+	 public function logout()
+    {
+       Session::destroy();
+		 AbsView::site_redirect('/');
+    }
 	
-	
-	public function getLogin()
-	{
-		SessionHelpers::getUserData($Login);
-	}
-
-	public function logOut()
-	{
-		SessionHelpers::destroyUserData();
-		AbsView::site_redirect('/');
-   
-	}
 }
